8a133556fa270ac42bc44676d7dafbbaffc40f58
Liam <liam.aharon@hotmail.com>
2017-03-13 15:54:17 +1100

    implemented DFS as per specification

--- program/traverse.c
+++ program/traverse.c
@@ -1,10 +1,54 @@
-/* ... */
+/* * * * * * *
+ * Module for functions that solve the coding tasks from assignment 1
+ *
+ * created for COMP20007 Design of Algorithms - Assignment 1, 2017
+ * by Liam Aharon <laharon@student.unimelb.edu.au>
+ */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "traverse.h"
 
+/* function definitions */
+
 void print_dfs(Graph* graph, int source_id) {
-	printf("not yet implemented: put code for part 1 here\n");
+  const int MAX_VERTICES = graph->maxn;
+
+  // array containing 1 or 0 depending whether or not a vertex has been visited
+  int* visited = calloc(sizeof(int), MAX_VERTICES);
+
+  // stack to contain vertices as they're checked
+  Stack* stack = new_stack(MAX_VERTICES);
+
+  // push origin node_id to top of stack, mark visited and print
+  push_stack(stack, source_id);
+  visited[source_id] = 1;
+  printf("%s\n", graph->vertices[source_id]->label);
+
+  // keep looking through vertices until stack is empty
+  while (stack->front != -1) {
+    // check every edge leading from the vertice ontop of the stack
+
+    Edge* edge = graph->vertices[peek_stack(stack)]->first_edge;
+    while (edge != NULL) {
+      // if we find an unvisited vertice push to top of the stack and restart
+      // process
+      if (visited[edge->v] == 0) {
+        printf("%s\n", graph->vertices[edge->v]->label);
+        visited[edge->v] = 1;
+        push_stack(stack, edge->v);
+        break;
+      }
+      // if vertice leading from edge already seen check the next edge.
+      // if seen all vertices remove vertice from stack
+      else {
+        edge = edge->next_edge;
+        if (edge == NULL) pop_stack(stack);
+      }
+    }
+  }
+  free_stack(stack);
+  free(visited);
 }
 
 void print_bfs(Graph* graph, int source_id) {
